cb68d82de64c3e1da5e09d0d212191b714dfd3e4
unknown <mercadosepulveda@gmail.com>
2024-06-16 23:08:11 -0400

    comit 6 corrección seguridad

--- app/core/config.py
+++ app/core/config.py
@@ -7,10 +7,6 @@ load_dotenv()  # Asegúrate de que esta línea esté al principio para cargar la
 
 class Settings:
     SECRET_KEY: str = os.getenv("SECRET_KEY", "default_secret_key")
-    #DATABASE_URL = "postgresql+asyncpg://rmercado:rm1802@190.114.255.158/camiones"
-    #DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:P%40stgr%40s2024@45.236.131.81/camiones")
-    #DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/camiones")
-    #DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://rmercado:rm1802@190.114.255.158/camiones")
     DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/gapo")
 settings = Settings()  # Esta es la instancia de la clase que estás exportando
  
